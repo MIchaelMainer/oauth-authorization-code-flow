e995051095443eb99eae171a17adcfb29b1fd78f
Caitlin Bales (MSFT) <caitbal@microsoft.com>
2017-01-27 15:02:27 -0800

    Add login route

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/data', 'DataController@showUserInfo');
+Route::get('/login', 'LoginController@login');
 Route::get('/setLogin', 'LoginController@setLogin');
